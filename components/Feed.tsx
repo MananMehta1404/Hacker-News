47d5dbef34ec049e03ba085b9b42cc774dc45f93
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-10-23 14:54:51 +0530

    Function to fetch posts and updating the states

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,16 +1,48 @@
 "use client"
 
+import { fetchPosts } from '@/utils';
 import Image from 'next/image';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Feed = () => {
 
+  // Searching and Loading related states
   const [searchText, setSearchText] = useState('');
   const [loading, setLoading] = useState(false);
 
+  // Post related states
+  const [allPosts, setAllPosts] = useState(Object);
+  const [pageNo, setpageNo] = useState(0);
+  const [nPages, setNPages] = useState(0);
+
+  // Function to fetch posts
+  const getPosts = async () => {
+    setLoading(true);
+
+    try{
+      const result = await fetchPosts(searchText, pageNo);
+
+      // Updating states
+      setAllPosts(result);
+      setpageNo(result.page);
+      setNPages(result.nbPages);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  // Function to handle search
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
   }
+
+  // Fetching posts when searchText or pageNo changes
+  useEffect(() => {
+    getPosts();
+  }, [searchText, pageNo]);
+
 
   return (
     <section className='feed'>
