6b36470298d4c0bd4e0182c6d644757cf868fc64
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-10-23 14:42:38 +0530

    Implemented the searching & loading functionality

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+"use client"
+
+import Image from 'next/image';
+import { useState } from 'react'
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  }
+
   return (
-    <div>
-      Feed
-    </div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'> 
+        <input
+          type='text'
+          placeholder='Search stories by title, url or author'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      {loading && (
+        <div className='mt-16 w-full flex-center'>
+          <Image
+            src='/loader.svg'
+            alt='loader'
+            width={50}
+            height={50}
+            className='object-contain'
+          />
+        </div>
+      )}
+    </section>
   )
 }
 
